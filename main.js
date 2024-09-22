0beab9f34c06891014d817333350ae7097c0081a
Rehab Sameh <161374560+rehabsamh99@users.noreply.github.com>
2024-09-23 00:52:10 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,17 +4,21 @@ const buttons = document.querySelectorAll('button');
 buttons .forEach((item) => {
     item .onclick = () => {
         if(item .id == 'clear'){
-            display .innerText ='' 
-    } else if (item.id == 'delete'){
+        display .innerText ="";
+    } else if (item.id == "backspace") {
         let string = display.innerText.toString();
-        display.innerText = string.substr(0 , string.lenght - 1);
+        display.innerText = string.substr(0, string.length - 1);
+
     }else if (display.innerText != '' && item.id == 'equal'){
-        display.innerText = eval(display.innerText);
+    display.innerText = eval(display.innerText);
+
     }else if (display.innerText =='' && item.id =='equal'){
         display.innerText = 'Empty!';
         setTimeout(() => ( display.innerText = ''),2000);
+
     } else {
         display.innerText += item.id; 
     }
 } 
 })
+
